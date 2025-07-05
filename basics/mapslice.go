aa51afbbdf69ff49317aeb0c9a3260d76e038f1b
Craig Murray <cmurray4492@gmail.com>
2025-07-04 22:16:25 -0500

    First Basic Add

---
+++ basics/mapslice.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	myMap := make(map[string]string)
+
+	myMap["dog"] = "Samson"
+
+	fmt.Println(myMap["dog"])
+	fmt.Println("Craig D Murray")
+
+}
